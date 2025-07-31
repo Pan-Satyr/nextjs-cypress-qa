4c86c1189ab2631f8393b26104bfc58e63051ebb
Pan <alonsoram@gmail.com>
2025-07-31 11:29:10 -0600

    Refactor Button component to support data-testid and other props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import styles from "./button.module.css";
-import Link from "next/link";
+import React from 'react';
+import styles from './button.module.css';
+import Link from 'next/link';
 
-const Button = ({ text, url }) => {
+const Button = ({ text, url, ...props }) => {
   return (
     <Link href={url}>
-      <button className={styles.container}>{text}</button>
+      <button className={styles.container} {...props}>
+        {text}
+      </button>
     </Link>
   );
 };
