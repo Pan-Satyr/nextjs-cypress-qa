706472265b00cb90dcb9e5614e1ba817b7e9de56
Pan <alonsoram@gmail.com>
2025-07-31 12:13:43 -0600

    Added tests for dark theme toggle

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -7,6 +7,9 @@ describe('Home Page renders', () => {
     cy.getByTestId('home-title').should('contain.text', 'Better design');
     cy.getByTestId('home-description').should('exist');
     cy.getByTestId('home-hero-image').should('be.visible');
+    cy.getByTestId('theme-wrapper').should('have.class', 'dark');
+    cy.getByTestId('theme-toggle-button').click();
+    cy.getByTestId('theme-wrapper').should('have.class', 'light');
   });
 
   it('navigates to portfolio page when CTA is clicked', () => {
